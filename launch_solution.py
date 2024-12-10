3445e5e798f63633e40467cfa3bf3f6854251ebe
lane_kubicki <lane.kubicki@gmail.com>
2024-12-10 16:17:00 -0500

    ADD DAY 4 PART 2 2024
    - add day4part2 and cleanup unused imports from other files i noticed

--- launch_solution.py
+++ launch_solution.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from advent_of_code_2024.day10 import AdventCode, DAY
+from advent_of_code_2024.day04 import AdventCode, DAY
 
 
 def read_input_file(dest):
@@ -8,14 +8,16 @@ def read_input_file(dest):
 
 
 if __name__ == "__main__":
-    test_input = """89010123
-78121874
-87430965
-96549874
-45678903
-32019012
-01329801
-10456732"""
+    test_input = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
     test_input = read_input_file(f"advent_of_code_2024/inputs/day{DAY}.txt")
     xmas = AdventCode()
     result = xmas.part2(test_input)
