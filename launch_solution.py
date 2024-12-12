a082fcf674a6ec900ffbeadff9996a77fd3983b0
lane_kubicki <lane.kubicki@gmail.com>
2024-12-12 00:02:01 -0500

    - day6 part1 25m

--- launch_solution.py
+++ launch_solution.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from advent_of_code_2024.day05 import AdventCode, DAY
+from advent_of_code_2024.day06 import AdventCode, DAY
 
 
 def read_input_file(dest):
@@ -8,35 +8,17 @@ def read_input_file(dest):
 
 
 if __name__ == "__main__":
-    test_input = """47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47"""  # """0 1 10 99 999"""
+    test_input = """....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#..."""  # """0 1 10 99 999"""
     test_input = read_input_file(f"advent_of_code_2024/inputs/day{DAY}.txt")
     xmas = AdventCode()
-    result = xmas.part2(test_input)
+    result = xmas.part1(test_input)
     print(f"Complete! Result = {result}")
