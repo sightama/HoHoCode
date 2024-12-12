c0370d238f9563ae1fff0fd8ea44f6aed8895e91
lane_kubicki <lane.kubicki@gmail.com>
2024-12-11 23:22:44 -0500

    - add day5part2 (catching up slowly) p1 took 37 minutes p2 took 17 minutes. not proud of part2 code, kinda slow brute forcey ew but works

--- advent_of_code_2024/day05.py
+++ advent_of_code_2024/day05.py
@@ -6,8 +6,11 @@ DAY = 5
 
 
 class AdventCode:
-
-    def part1(self, inn: str):
+    # first parse "rules", and also the "updates"
+    # check all rules against each line. two counters, each update is short/finite. sliding window left|rght
+    # if all rules pass, get middle value and add to total_valuee
+    # return total value
+    def part1(self, inn: str):  # 37m
         inn = inn.split('\n')
         total_value = 0
         rules_str = deque()
@@ -31,15 +34,10 @@ class AdventCode:
             current = current.split(',')
             valid = self.check_rules(current, deepcopy(rules))
             if valid:
-                mid_indice = len(current)//2
+                mid_indice = len(current) // 2
                 total_value += int(current[mid_indice])  # middle element of list/deque
 
         return total_value
-
-        # first parse "rules", and also the "updates"
-        # check all rules against each line. two counters, each update is short/finite. sliding window left|rght
-        # if all rules pass, get middle value and add to total_valuee
-        # return total value
 
     @staticmethod
     def check_rules(update: List, rules: deque):
@@ -56,3 +54,48 @@ class AdventCode:
             if order[0] == 'r' and order[1] == 'l':
                 return False
         return True
+
+    def part2(self, inn: str):  # 17m
+        inn = inn.split('\n')
+        total_value = 0
+        rules_str = deque()
+        updates = deque()
+        for row in inn:
+            if '' == row:
+                continue
+            elif '|' in row:
+                rules_str.append(row)
+            elif ',' in row:
+                updates.append(row)
+
+        rules = deque()  # cant use dict same key values. use deque
+        for rule in rules_str:
+            sl = rule.split('|')
+            l, r = sl[0], sl[1]
+            rules.append((l, r))
+
+        while updates:
+            current = updates.popleft()
+            current = current.split(',')
+            valid = self.check_rules(current, deepcopy(rules))
+
+            if not valid:
+                fixed_order = self.fix_order(deepcopy(deque(current)), deepcopy(rules))
+                mid_indice = len(fixed_order) // 2
+                total_value += int(fixed_order[mid_indice])  # middle element of list/deque
+
+        return total_value
+
+    def fix_order(self, update: deque, rules: deque):
+        og_rules = deepcopy(rules)
+        while rules:  # first find all mistakes and their pos
+            l, r = rules.popleft()
+            if l in update and r in update:
+                l_index = update.index(l)
+                r_index = update.index(r)
+                if r_index < l_index:
+                    update[r_index] = l
+                    update[l_index] = r
+                    update = self.fix_order(update, og_rules)
+
+        return update
