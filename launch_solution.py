2844040d8d2afba26276c80c3e9537827680f873
lane_kubicki <lane.kubicki@gmail.com>
2024-12-12 15:05:31 -0500

    - FUCK ME THE BUG WAS USING A DICT WHERE KEYS WERE TEST_VALUE OVERWRITING IF MULTIPLE TEST_VALUE LINES EXIST.STUPIDSTUPIDSTUPID

--- launch_solution.py
+++ launch_solution.py
@@ -8,7 +8,8 @@ def read_input_file(dest):
 
 
 if __name__ == "__main__":
-    test_input = """190: 10 19
+    test_input = """15: 15 1 2
+190: 10 19
 3267: 81 40 27
 83: 17 5
 156: 15 6
@@ -21,10 +22,3 @@ if __name__ == "__main__":
     xmas = AdventCode()
     result = xmas.part1(test_input)
     print(f"Complete! Result = {result}")
-
-
-# why is this value too low = 3350727455032
-                            # 3351430525058
-                            # 3351424676480
-                            # 3351424676480
-# REAL ANSWER  3312271365652
