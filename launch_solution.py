5cd3f67c4801ad6216ba0558f13f9fb8f9ffd6f6
lane_kubicki <lane.kubicki@gmail.com>
2024-12-17 14:24:34 -0500

    - Working solution for day12part1 on ALL SAMPLES.....however still too high for actual input, so somewhere we are catching extra edges ew - Create solution for day13 part1 by myself weeeeee - Created solution for day13part2 but not working, combinatorial explosion. Did linear algebra googling and talked connor, use inverse matrix to find if there is determinant and from there find the answer using numpy.solve() (matrix  array with X,Y)

--- launch_solution.py
+++ launch_solution.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from advent_of_code_2024.day12 import AdventCode, DAY
+from advent_of_code_2024.day13 import AdventCode, DAY
 
 
 def read_input_file(dest):
@@ -8,12 +8,23 @@ def read_input_file(dest):
 
 
 if __name__ == "__main__":
-    test_input = """OOOOO
-OXOXO
-OOOOO
-OXOXO
-OOOOO"""
-    # test_input = read_input_file(f"advent_of_code_2024/inputs/day{DAY}.txt")
+    test_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
+    test_input = read_input_file(f"advent_of_code_2024/inputs/day{DAY}.txt")
     xmas = AdventCode()
-    result = xmas.part1(test_input)
+    result = xmas.part2(test_input)
     print(f"Complete! Result = {result}")
+# try asgain - 1538336
