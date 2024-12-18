ccb692685bdb6508c5c6ffaefb4ec0f0641bcee9
lane_kubicki <lane.kubicki@gmail.com>
2024-12-18 14:01:02 -0500

    - weewoooo day14 is fav thus far

--- launch_solution.py
+++ launch_solution.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from advent_of_code_2024.day14 import AdventCode, DAY
+from advent_of_code_2024.day18 import AdventCode, DAY
 
 
 def read_input_file(dest):
@@ -8,20 +8,32 @@ def read_input_file(dest):
 
 
 if __name__ == "__main__":
-    test_input = """p=0,4 v=3,-3
-p=6,3 v=-1,-3
-p=10,3 v=-1,2
-p=2,0 v=2,-1
-p=0,0 v=1,3
-p=3,0 v=-2,-2
-p=7,6 v=-1,-3
-p=3,0 v=-1,-2
-p=9,3 v=2,3
-p=7,3 v=-1,2
-p=2,4 v=2,-3
-p=9,5 v=-3,-3"""
-    # test_input = read_input_file(f"advent_of_code_2024/inputs/day{DAY}.txt")
+    test_input = """5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"""
+    test_input = read_input_file(f"advent_of_code_2024/inputs/day{DAY}.txt")
     xmas = AdventCode()
-    result = xmas.part1(test_input)
+    result = xmas.part2(test_input)
     print(f"Complete! Result = {result}")
-# try asgain - 1538336
