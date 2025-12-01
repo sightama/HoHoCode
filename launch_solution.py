618b1b01163c1426be440da7a7a5dda0dd6de5c4
Lane <lkubicki@dhdiagnostics.com>
2025-12-01 11:57:13 -0500

    update for day01, add new 2025 folder and day1 solution + time took

--- launch_solution.py
+++ launch_solution.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from advent_of_code_2024.day18 import AdventCode, DAY
+from advent_of_code_2025.day01 import AdventCode, DAY
 
 
 def read_input_file(dest):
@@ -8,32 +8,17 @@ def read_input_file(dest):
 
 
 if __name__ == "__main__":
-    test_input = """5,4
-4,2
-4,5
-3,0
-2,1
-6,3
-2,4
-1,5
-0,6
-3,3
-2,6
-5,1
-1,2
-5,5
-2,5
-6,5
-1,4
-0,4
-6,4
-1,1
-6,1
-1,0
-0,5
-1,6
-2,0"""
-    test_input = read_input_file(f"advent_of_code_2024/inputs/day{DAY}.txt")
+    test_input = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+    test_input = read_input_file(f"advent_of_code_2025/inputs/day{DAY}.txt")
     xmas = AdventCode()
     result = xmas.part2(test_input)
     print(f"Complete! Result = {result}")
