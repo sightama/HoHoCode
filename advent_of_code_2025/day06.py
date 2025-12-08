72541137e3d0be369025ea8798991a2af660cb3e
Lane <lkubicki@dhdiagnostics.com>
2025-12-08 18:45:57 -0500

    took hour and 10 minutes for this did really good. Not a lot of doubt on my solution

--- advent_of_code_2025/day06.py
+++ advent_of_code_2025/day06.py
@@ -33,6 +33,73 @@ class AdventCode:
             result += final
         return result
 
+
+    # new things: Start at grid[0] (top) and go down
+    # We know that you find the longest nums (len(num) == 3), get all nums that are 3 digits, go top to bottom.
+    # nevermind fuck trying above, its all whitespace movement.
+
+    # idea: Find the EXACT X pos of the operators. This represents where we END every calculation.
+    # To find the STARTING X point we go up and down on each char until we find the ONLY WHITESPACE column.
+    # From there we go, for every X-1, start at y==0 and do y+1 and check if ' ' skip, if not append y[i]. 
+    # at end do operator logic. seems simple eh?
+    # took 1 hour 10 minutes
     @staticmethod
     def part2(inn: str):
-        pass
+        grid = inn.strip().split('\n')
+        operations = grid[-1]
+        grid.remove(operations)
+        
+        operation_locations = [] # find operation locations (ie the "starting points")
+        for i, char in enumerate(operations):
+            if char in ['*', '+']:
+                operation_locations.append((i, char))
+        final_op = operation_locations.pop()  # no whitespace column for last equation.
+        endpoints = []
+        for x, char in operation_locations:  # the starting points, go right find where all whitespace.
+            all_white = True
+            final_x = x+1 # +1 we dont count the starting point, thats clearly not whitespace.
+            while all_white:
+                for y in range(len(grid)):
+                    if grid[y][final_x] != ' ':
+                        all_white = False
+                        break
+                if all_white:
+                    endpoints.append(final_x)
+                    break
+                else:  # This column WAS NOT our final column. try again on next.
+                    all_white = True
+                    final_x = final_x + 1
+        # edge case: There is no whitespace for the final column? What do? I think just append lenx,leny to end (4 start 3 stops + length)
+        endpoints.append(len(grid[0]))  # not -1 because we subtract -1 for whitespace from all.
+        operation_locations.append(final_op)
+        x_cases = deque()
+        for tupl, end_x in zip(operation_locations, endpoints):
+            x, char = tupl
+            x_cases.append((x, end_x, char))
+        total = 0
+        # NOW we do each equation and compute on-the-fly
+        while x_cases:
+            all_values_for_op = []
+            start, end, op = x_cases.popleft()
+            end -= 1 # to account for whitespace column,
+            while end != start - 1:
+                num = ''
+                for y in range(len(grid)):
+                    if grid[y][end] == ' ':
+                        continue
+                    else:
+                        num = num + grid[y][end]
+                if num and not num.isspace():
+                    all_values_for_op.append(int(num))
+                end -= 1
+            # we have our nums, now do op.
+            if op == '*':
+                subtotal = 1
+                for num in all_values_for_op:
+                    subtotal *= num
+            if op == '+':
+                subtotal = 0
+                for num in all_values_for_op:
+                    subtotal += num
+            total += subtotal
+        return total
--- launch_solution.py
+++ launch_solution.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
 *   +   *   +  """
     test_input = read_input_file(f"advent_of_code_2025/inputs/day{DAY}.txt")
     xmas = AdventCode()
-    result = xmas.part1(test_input)
+    result = xmas.part2(test_input)
     print(f"Complete! Result = {result}")
